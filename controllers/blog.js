8e0be172dec1aebf3055461787999052ff22c1dc
pratiksj <pratiksharai728@gmail.com>
2023-10-08 16:09:18 +0545

    feat:complete till 13.12 exercise

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,18 +1,33 @@
 
 const blogRouter = require('express').Router()
-const { Blog } = require('../model')
-const { blogFinder } = require('../util/middle')
+const { Blog, User } = require('../model')
+const { blogFinder, tokenExtrator } = require('../util/middle')
 
-blogRouter.get('/', async (req, res) => {
+blogRouter.get('/', async (req, res, next) => {
+    try {
+        const blogs = await Blog.findAll({
+            include: {
+                model: User,
+                attributes: ['name']
+            }
+        })
+        res.json(blogs)
+    } catch (error) { next(error) }
 
-    const blogs = await Blog.findAll()
-    res.json(blogs)
 })
 
-blogRouter.post('/', async (req, res, next) => {
+blogRouter.post('/', tokenExtrator, async (req, res, next) => {
     try {
-        const blog = await Blog.create(req.body)
+
+        const user = await User.findByPk(req.decodedToken.id)
+
+        const blog = Blog.build({ ...req.body })
+        blog.userId = user.id
+        await blog.save()
         res.json(blog)
+
+        //const blog = await Blog.create(req.body)
+        //res.json(blog)
     } catch (error) {
         next(error)
     }
@@ -27,6 +42,22 @@ blogRouter.put('/:id', blogFinder, async (req, res, next) => {
     } catch (error) {
         next(error)
     }
+
+
+})
+
+blogRouter.delete('/:id', tokenExtrator, blogFinder, async (req, res, next) => {
+    try {
+        const blogToDelete = req.blog
+        if (req.blog.userId !== req.decodedToken.id) {
+            res.status(404).json({ message: 'you are note authorized to delete this note' })
+        }
+        else {
+            await blogToDelete.destroy()
+            res.status(204).end()
+        }
+
+    } catch (error) { next(error) }
 
 
 })
--- util/middle.js
+++ util/middle.js
@@ -1,4 +1,6 @@
 const { Blog } = require('../model')
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('../util/config')
 
 const blogFinder = async (req, res, next) => {
     req.blog = await Blog.findByPk(req.params.id)
@@ -11,8 +13,25 @@ const errorHandler = (error, request, response, next) => {
     if (error.name === "SequelizeValidationError") {
         return response.status(400).send({ error: error.message })
     }
+    if (error.name === 'SequelizeDatabaseError') {
+        return response.status(400).send({ error: error.message })
+    }
     //response.status(500).json({ error: 'Internal server Error' })
     next(error)
 }
+const tokenExtrator = (req, res, next) => {
+    const authorization = req.get('authorization')
+    console.log(authorization, 'authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        try {
+            req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+        } catch {
+            return res.status(401).json({ error: 'token invalid' })
+        }
+    } else {
+        return res.status(401).json({ error: 'token missing' })
+    }
+    next()
+}
 
-module.exports = { blogFinder, errorHandler }
+module.exports = { blogFinder, errorHandler, tokenExtrator }
